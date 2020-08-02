bac5e2cca51f4efeecbd2217af5ddcf505d094b8
George Alan <georgealanrufo@gmail.com>
2020-08-02 20:14:48 -0300

    Adiconado classes do projeto, teste de commit no wsl2 com Jetbrains.

---
+++ src/DrawPanel.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class DrawPanel extends JPanel {
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+
+        int width = getWidth();
+        int height = getHeight();
+
+        g.drawLine(0, 0, width, height);
+        g.drawLine(0, height, width, 0);
+    }
+}
