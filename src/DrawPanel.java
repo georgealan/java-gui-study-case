11d26d7e31daa17cd5dbfc3a2c31446b7b2aa1fc
George Alan <georgealanrufo@gmail.com>
2020-08-02 20:31:29 -0300

    Ajustes

--- src/DrawPanel.java
+++ src/DrawPanel.java
@@ -8,8 +8,8 @@ public class DrawPanel extends JPanel {
         int width = getWidth();
         int height = getHeight();
 
-//        g.drawLine(200, 300, width, height);
-//        g.drawLine(0, 300, width, height);
-        //g.drawLine(4000, height, width, 1230);
+        g.drawLine(200, 300, width, height);
+        g.drawLine(0, 300, width, height);
+        g.drawLine(4000, height, width, 1230);
     }
 }
