b6952323915c4d4c2ca20aa6f03fc8b043899536
George Alan Rufo <georgealanrufo@gmail.com>
2020-08-03 00:00:37 -0300

    resolvido a sequencia de 3 cantos falta 1 e depois otimizar com um laço de repetição

--- src/DrawPanel.java
+++ src/DrawPanel.java
@@ -8,17 +8,81 @@ public class DrawPanel extends JPanel {
         int width = getWidth();
         int height = getHeight();
 
-        g.drawLine(0, 0, width, height);
-        g.drawLine(0, 10, width, height);
-        g.drawLine(0, 20, width, height);
-        g.drawLine(0, 30, width, height);
-        g.drawLine(0, 40, width, height);
-        g.drawLine(0, 50, width, height);
-        g.drawLine(10, 0, width, height);
-        g.drawLine(20, 0, width, height);
-        g.drawLine(30, 0, width, height);
-        g.drawLine(40, 0, width, height);
-        g.drawLine(50, 0, width, height);
-        g.drawLine(100, height, width, 15);
+        // INICIO LADO SUPERIOR ESQUERDO
+        g.drawLine(0, 0, width / 2, height / 2);
+        g.drawLine(0, 0, width / 2 - 30, height / 2 + 30);
+        g.drawLine(0, 0, width / 2 - 60, height / 2 + 60);
+        g.drawLine(0, 0, width / 2 - 90, height / 2 + 90);
+        g.drawLine(0, 0, width / 2 - 120, height / 2 + 120);
+        g.drawLine(0, 0, width / 2 - 150, height / 2 + 150);
+        g.drawLine(0, 0, width / 2 - 180, height / 2 + 180);
+        g.drawLine(0, 0, width / 2 - 210, height / 2 + 210);
+
+        g.drawLine(0, 0, width / 2 + 30, height / 2 - 30);
+        g.drawLine(0, 0, width / 2 + 60, height / 2 - 60);
+        g.drawLine(0, 0, width / 2 + 90, height / 2 - 90);
+        g.drawLine(0, 0, width / 2 + 120, height / 2 - 120);
+        g.drawLine(0, 0, width / 2 + 150, height / 2 - 150);
+        g.drawLine(0, 0, width / 2 + 180, height / 2 - 180);
+        g.drawLine(0, 0, width / 2 + 210, height / 2 - 210);
+        // FIM LADO SUPERIOR ESQUERDO
+
+        // INICIO LADO SUPERIOR DIREITO
+//        g.drawLine(height / 2, 0, 0, height);
+//        g.drawLine(0, 0, width / 2 - 30, height / 2 + 30);
+//        g.drawLine(0, 0, width / 2 - 60, height / 2 + 60);
+//        g.drawLine(0, 0, width / 2 - 90, height / 2 + 90);
+//        g.drawLine(0, 0, width / 2 - 120, height / 2 + 120);
+//        g.drawLine(0, 0, width / 2 - 150, height / 2 + 150);
+//        g.drawLine(0, 0, width / 2 - 180, height / 2 + 180);
+//        g.drawLine(0, 0, width / 2 - 210, height / 2 + 210);
+
+//        g.drawLine(0, 0, width / 2 + 30, height / 2 - 30);
+//        g.drawLine(0, 0, width / 2 + 60, height / 2 - 60);
+//        g.drawLine(0, 0, width / 2 + 90, height / 2 - 90);
+//        g.drawLine(0, 0, width / 2 + 120, height / 2 - 120);
+//        g.drawLine(0, 0, width / 2 + 150, height / 2 - 150);
+//        g.drawLine(0, 0, width / 2 + 180, height / 2 - 180);
+//        g.drawLine(0, 0, width / 2 + 210, height / 2 - 210);
+        // FIM LADO SUPERIOR DIREITO
+
+        // INICIO LADO INFERIOR DIREITO
+        g.drawLine(width / 2, height / 2, width, height);
+        g.drawLine(width / 2 - 30, height / 2 + 30, width, height);
+        g.drawLine(width / 2 - 60, height / 2 + 60, width, height);
+        g.drawLine(width / 2 - 90, height / 2 + 90, width, height);
+        g.drawLine(width / 2 - 120, height / 2 + 120, width, height);
+        g.drawLine(width / 2 - 150, height / 2 + 150, width, height);
+        g.drawLine(width / 2 - 180, height / 2 + 180, width, height);
+        g.drawLine(width / 2 - 210, height / 2 + 210, width, height);
+
+        g.drawLine( width / 2 + 30, height / 2 - 30, width, height);
+        g.drawLine( width / 2 + 60, height / 2 - 60, width, height);
+        g.drawLine( width / 2 + 90, height / 2 - 90, width, height);
+        g.drawLine( width / 2 + 120, height / 2 - 120, width, height);
+        g.drawLine( width / 2 + 150, height / 2 - 150, width, height);
+        g.drawLine( width / 2 + 180, height / 2 - 180, width, height);
+        g.drawLine( width / 2 + 210, height / 2 - 210, width, height);
+        // FIM LADO INFERIOR DIREITO
+
+        // INICIO LADO INFERIOR ESQUERDO
+        g.drawLine(0, height, width / 2, height / 2);
+        g.drawLine(0, height, width / 2 - 30, height / 2 - 30);
+        g.drawLine(0, height, width / 2 - 60, height / 2 - 60);
+        g.drawLine(0, height, width / 2 - 90, height / 2 - 90);
+        g.drawLine(0, height, width / 2 - 120, height / 2 - 120);
+        g.drawLine(0, height, width / 2 - 150, height / 2 - 150);
+        g.drawLine(0, height, width / 2 - 180, height / 2 - 180);
+        g.drawLine(0, height, width / 2 - 210, height / 2 - 210);
+
+        g.drawLine(0, height, width / 2, height / 2);
+        g.drawLine(0, height, width / 2 + 30, height / 2 + 30);
+        g.drawLine(0, height, width / 2 + 60, height / 2 + 60);
+        g.drawLine(0, height, width / 2 + 90, height / 2 + 90);
+        g.drawLine(0, height, width / 2 + 120, height / 2 + 120);
+        g.drawLine(0, height, width / 2 + 150, height / 2 + 150);
+        g.drawLine(0, height, width / 2 + 180, height / 2 + 180);
+        g.drawLine(0, height, width / 2 + 210, height / 2 + 210);
+        // FIM LADO INFERIOR ESQUERDO
     }
 }
